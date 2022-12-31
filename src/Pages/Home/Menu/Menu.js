893952a9f278500cacfffed8065684c427ce0034
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-01-01 02:10:27 +0600

    add 3 menu item and add link

---
+++ src/Pages/Home/Menu/Menu.js
@@ -0,0 +1,44 @@
+import React from "react";
+import './Menu.css'
+import { Link } from "react-router-dom";
+import Breakfast from '../../../Assets/Menu/Breakfast.png'
+import Pastry from '../../../Assets/Menu/Pastry.png'
+import Cake from '../../../Assets/Menu/Cofee Cake.png'
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+
+const Menu = () => {
+    return (
+        <div className="grid lg:grid-cols-3 gap-10 mt-10 mx-12">
+            <div className="menu1">
+                <div className="menu-image">
+                    <img src={Pastry} alt="" />
+                </div>
+                <p className="menu-name">
+                    <Link to="/abbo" className="menu-link">Pastry</Link>
+                    <p className="arro-icon"><HiOutlineArrowNarrowRight className="mr-5" /> <span>(44)</span></p>
+                </p>
+            </div>
+            <div className="menu1">
+                <div className="menu-image">
+                    <img src={Breakfast} alt="" />
+                </div>
+                <p className="menu-name">
+                    <Link to="/abbo" className="menu-link">Breakfast</Link>
+                    <p className="arro-icon"><HiOutlineArrowNarrowRight className="mr-5" /> <span>(44)</span></p>
+                </p>
+            </div>
+            <div className="menu1">
+                <div className="menu-image">
+                    <img src={Cake} alt="" />
+                </div>
+                <p className="menu-name">
+                    <Link to="/abbo" className="menu-link">Cake</Link>
+                    <p className="arro-icon"><HiOutlineArrowNarrowRight className="mr-5" /> <span>(44)</span></p>
+                </p>
+            </div>
+        </div>
+
+    );
+};
+
+export default Menu;
