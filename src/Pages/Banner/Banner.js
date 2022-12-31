053f214e6040f5d5b43151ec4ab3846deee67a49
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-12-31 23:12:41 +0600

    add banner part where add bg picture and add some text

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -3,7 +3,11 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div className="banner mt-5">
-            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum deserunt ullam adipisci, velit dolorum possimus veniam dolorem reiciendis, quod corporis doloremque libero? Tempore modi obcaecati consequatur, incidunt ipsum cupiditate culpa!</h1>
+            <div className="banner-contant">
+                <h1 className="offer-title">UP TO SALE <span className="offer-persen">50% OFF</span></h1>
+                <p className="banner-title">WE BAKE WITH <br /> PASION.</p>
+                <button className="shop-btn">SHOP NOW</button>
+            </div>
 
         </div>
     );
