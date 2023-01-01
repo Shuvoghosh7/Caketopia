2dd03789abc67abd6c34ed1a7fb503709444b8bd
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-01-02 00:29:39 +0600

    add product amount and set localstore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import './App.css';
 import { useState } from 'react';
 import Navbar from './Component/Navbar/Navbar';
 import TopNav from './Component/Navbar/TopNav';
@@ -7,16 +8,15 @@ import BestSellers from './Pages/Products/BestSellers/BestSellers';
 import NewItems from './Pages/Products/NewItems/NewItems';
 import OurFeatures from './Pages/Products/OurFeatures/OurFeatures';
 import ShopCart from './Pages/ShopCart/ShopCart';
+
 function App() {
-  let x = localStorage.getItem("cartss")
-  let y = JSON.parse(x)
-  console.log(y)
+  let getCartData = localStorage.getItem("cartss")
+  let storeCartData = JSON.parse(getCartData)
   const [cart, setCart] = useState([]);
   const [warning, setWarning] = useState(false);
-
   const handleClick = (item) => {
     let isPresent = false;
-    cart.forEach((product) => {
+    storeCartData.forEach((product) => {
       if (item.id === product.id)
         isPresent = true;
     })
@@ -24,44 +24,44 @@ function App() {
       setWarning(true);
       setTimeout(() => {
         setWarning(false);
-      }, 2000);
+      }, 3000);
       return;
     }
     setCart([...cart, item]);
-    const x = [...cart, item];
-    localStorage.setItem("cartss", JSON.stringify(x))
+    const cartItem = [...cart, item];
+    localStorage.setItem("cartss", JSON.stringify(cartItem))
   }
 
   const handleChange = (item, d) => {
     let ind = -1;
-    cart.forEach((data, index) => {
+    storeCartData.forEach((data, index) => {
       if (data.id === item.id)
         ind = index;
     });
-    const tempArr = cart;
+    const tempArr = storeCartData;
     tempArr[ind].amount += d;
 
     if (tempArr[ind].amount === 0)
       tempArr[ind].amount = 1;
     setCart([...tempArr])
+    let dd = [...tempArr]
+    localStorage.setItem("cartss", JSON.stringify(dd))
   }
-  
   return (
     <div>
       <TopNav />
-      <Navbar size={y.length} />
+      <Navbar size={storeCartData.length} />
       <Routes>
         <Route path='/' element={<Home />}>
           <Route path='OurFeatures' element={<OurFeatures handleClick={handleClick} />} />
           <Route path='bestSellers' element={<BestSellers />} />
           <Route path='newItems' element={<NewItems />} />
         </Route>
-
         <Route path='/shopCart' element={<ShopCart cart={cart} setCart={setCart} handleChange={handleChange} />} />
-
-
-
       </Routes>
+      {
+        warning && <div className='warning'>Item is already added to your cart</div>
+      }
     </div>
   );
 }
