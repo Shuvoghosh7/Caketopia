154730bf1d617f6c663cc8fcfa25f3f0b05d42aa
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-01-15 21:25:33 +0600

    change require auth fatch process

--- src/Component/RequireAuth/RequireAuth.js
+++ src/Component/RequireAuth/RequireAuth.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { Navigate, useLocation,useHistory } from 'react-router-dom';
-
+import { Bars } from 'react-loader-spinner'
 const RequireAuth = ({ children }) => {
-    const [auth, setAuth] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
     const jsonToken = localStorage.getItem('token')
     const tokenParse = JSON.parse(jsonToken)
     let location = useLocation();
-    useEffect(() => {
-        fetch('https://caketopia-server-production.up.railway.app/api/v1/user/me', {
+
+    const { isLoading, error, data: auth } = useQuery('auth', () =>
+        fetch('http://localhost:5000/api/v1/user/me',{
             method: "GET",
             headers: {
                 'authorization': `Bearer ${tokenParse}`
             }
         })
-            .then(res => res.json())
-            .then(data => setAuth(data.data))
-    }, [])
-
+        .then(res =>res.json())
+    )
+    console.log(auth)
     if (isLoading) {
-        return setIsLoading(true)
+        return <div className='flex justify-center items-center'>
+            <Bars
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="bars-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />
+        </div>
     }
 
-     if (!auth) {
+     if (!auth.data) {
          return <Navigate to="/singin" state={{ from: location }} replace />;
        }
    
