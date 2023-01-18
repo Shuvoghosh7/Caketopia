dd60e20772ae5dca761eb578293d3e1e64ad200b
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-01-19 00:40:49 +0600

    application search functionality all product

--- src/Pages/Dashboard/AllProduct/AllProduct.js
+++ src/Pages/Dashboard/AllProduct/AllProduct.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import AllProductCard from './AllProductCard';
 import { Bars } from 'react-loader-spinner'
 const AllProduct = ({ handleClick }) => {
+    const [filter, setFilter] = useState('')
     const { isLoading, error, data: products } = useQuery('products', () =>
         fetch('https://caketopia-server-production.up.railway.app/api/v1/product').then(res =>
             res.json()
@@ -22,16 +23,30 @@ const AllProduct = ({ handleClick }) => {
             />
         </div>
     }
+    const searchHandeal = (event) => {
+        setFilter(event.target.value)
+    }
+    let dataSearch = products.filter(item => {
+        return Object.keys(item).some(key =>
+            item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
+        )
+    })
     return (
-        <div className='grid lg:grid-cols-3 gap-10 mx-12 mt-10'>
-            {
-                products?.map(Feature => <AllProductCard
-                    key={Feature.id}
-                    Feature={Feature}
-                    handleClick={handleClick}
-                />)
-            }
-        </div>
+        <>
+            <div className='flex lg:justify-end md:justify-end justify-center lg:mr-16 md:mr-16 mt-5'>
+                <input type="search" name="" id="" className="input input-bordered bg-[#EAE0DA] w-60" placeholder='search by name' value={filter} onChange={searchHandeal.bind(this)} />
+            </div>
+            <div className='grid lg:grid-cols-3 
+            md:grid-cols-2  gap-10 mx-12 mt-10'>
+                {
+                    dataSearch.length > 0 ? dataSearch?.map(Feature => <AllProductCard
+                        key={Feature.id}
+                        Feature={Feature}
+                        handleClick={handleClick}
+                    />) : <h1 className='relative lg:left-[400px] text-3xl'>No Result Found</h1>
+                }
+            </div>
+        </>
     );
 };
 
--- src/Pages/Products/OurFeatures/OurFeatures.js
+++ src/Pages/Products/OurFeatures/OurFeatures.js
@@ -3,13 +3,21 @@ import FeaturesCard from './FeaturesCard';
 import { useQuery } from 'react-query';
 import { Bars } from 'react-loader-spinner'
 const OurFeatures = ({ handleClick }) => {
-
+    const [filter, setFilter] = useState('')
     const { isLoading, error, data: products } = useQuery('products', () =>
         fetch('https://caketopia-server-production.up.railway.app/api/v1/product').then(res =>
             res.json()
         )
     )
 
+    const searchHandeal = (event) => {
+        setFilter(event.target.value)
+    }
+    let dataSearch = products.filter(item => {
+        return Object.keys(item).some(key =>
+            item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
+        )
+    })
     if (isLoading) {
         return <div className='flex justify-center'>
             <Bars
@@ -23,17 +31,22 @@ const OurFeatures = ({ handleClick }) => {
             />
         </div>
     }
-
+    
     return (
-        <div className='grid lg:grid-cols-4 gap-10 mx-12 mt-10'>
-            {
-                products?.map(Feature => <FeaturesCard
-                    key={Feature.id}
-                    Feature={Feature}
-                    handleClick={handleClick}
-                />)
-            }
-        </div>
+        <>
+            <div className='flex lg:justify-end md:justify-end justify-center lg:mr-12 md:mr-12 mt-3'>
+                <input type="search" name="" id="" className="input input-bordered bg-[#EAE0DA] w-60" placeholder='search by name' value={filter} onChange={searchHandeal.bind(this)} />
+            </div>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-10 mx-12 mt-10'>
+                {
+                    dataSearch.length > 0 ? dataSearch?.map(Feature => <FeaturesCard
+                        key={Feature.id}
+                        Feature={Feature}
+                        handleClick={handleClick}
+                    />):<h1 className='relative lg:left-[400px] text-3xl'>No Result Found</h1>
+                }
+            </div>
+        </>
     );
 };
 
